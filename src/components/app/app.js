2b7b7ec12ff83ec5769751e3bac7e93a08667d14
Nikita <n.torbinskiy@gmail.com>
2021-02-24 11:52:44 +0200

    some fixes

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,18 +7,18 @@ import PostAddForm from "../post_add_form";
 import './app.css';
 
 const App = () => {
-    return (
-        <div>
-            <AppHeader />
-            <div className="search-panel d-flex">
-                <SearchPanel />
-                <PostStatusFilter />
-            </div>
-            <PostList/>
-            <PostAddForm/>
-        </div>
+	return (
+		<div className="container">
+			<AppHeader />
+			<div className="search-panel d-flex">
+				<SearchPanel />
+				<PostStatusFilter />
+			</div>
+			<PostList />
+			<PostAddForm />
+		</div>
 
-    )
+	)
 }
 
 export default App;
